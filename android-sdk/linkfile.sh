eb9de3c157de812ca0b2d55f0dd771f491b492e1
Jeff Chiu <jeff@jeff-3000N100.(none)>
2012-04-28 21:57:30 +0800

    initial linux checkin

--- android-sdk/linkfile.sh
+++ android-sdk/linkfile.sh
@@ -1,25 +1,57 @@
-cd ../AccelerometerPlay/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../ApiDemos/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../BackupRestore/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../BasicGLSurfaceView/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../ContactManager/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../CubeLiveWallpaper/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../FileBrowser/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../GestureBuilder/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../HelloActivity/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../HelloViews/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../Home/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../JetBoy/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../LunarLander/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../MapsDemo/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../MarketLicensing/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../MultiResolution/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../NFCDemo/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../NotePad/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../PhoneDialer/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../SearchableDictionary/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../Snake/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../Support4Demos/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../TicTacToeLib/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../TicTacToeMain/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
-cd ../Wiktionary/ ; ln -s ../configs/ant/ant-windows.properties ant.properties; ln -s ../configs/ant/build-scala.xml .
+#
+#cd ../AccelerometerPlay/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../ApiDemos/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../BackupRestore/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../BasicGLSurfaceView/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../ContactManager/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../CubeLiveWallpaper/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../FileBrowser/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../GestureBuilder/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../HelloActivity/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../HelloViews/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../Home/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../JetBoy/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../LunarLander/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../MapsDemo/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../MarketLicensing/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../MultiResolution/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../NFCDemo/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../NotePad/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../PhoneDialer/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../SearchableDictionary/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../Snake/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../Support4Demos/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../TicTacToeLib/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../TicTacToeMain/;	 ln -s ../configs/ant/build-scala.xml .
+#cd ../Wiktionary/;	 ln -s ../configs/ant/build-scala.xml .
+#
+
+#Select one (must cygwin shell in windows for the symlink)
+#ln -s ../configs/ant/ant-windows.properties ../ant.properties
+#ln -s ../configs/ant/ant-unix.properties ../ant.properties
+
+cd ../AccelerometerPlay;	ln -s ../ant.properties ant.properties
+cd ../ApiDemos;			ln -s ../ant.properties ant.properties
+cd ../BackupRestore;		ln -s ../ant.properties ant.properties
+cd ../BasicGLSurfaceView;	ln -s ../ant.properties ant.properties
+cd ../ContactManager;		ln -s ../ant.properties ant.properties
+cd ../CubeLiveWallpaper;	ln -s ../ant.properties ant.properties
+cd ../FileBrowser;		ln -s ../ant.properties ant.properties
+cd ../GestureBuilder;		ln -s ../ant.properties ant.properties
+cd ../HelloActivity;		ln -s ../ant.properties ant.properties
+cd ../HelloViews;		ln -s ../ant.properties ant.properties
+cd ../Home;			ln -s ../ant.properties ant.properties
+cd ../JetBoy;			ln -s ../ant.properties ant.properties
+cd ../LunarLander;		ln -s ../ant.properties ant.properties
+cd ../MapsDemo;			ln -s ../ant.properties ant.properties
+cd ../MarketLicensing;		ln -s ../ant.properties ant.properties
+cd ../MultiResolution;		ln -s ../ant.properties ant.properties
+cd ../NFCDemo;			ln -s ../ant.properties ant.properties
+cd ../NotePad;			ln -s ../ant.properties ant.properties
+cd ../PhoneDialer;		ln -s ../ant.properties ant.properties
+cd ../SearchableDictionary;	ln -s ../ant.properties ant.properties
+cd ../Snake;			ln -s ../ant.properties ant.properties
+cd ../Support4Demos;		ln -s ../ant.properties ant.properties
+cd ../TicTacToeLib;		ln -s ../ant.properties ant.properties
+cd ../TicTacToeMain;		ln -s ../ant.properties ant.properties
+cd ../Wiktionary;		ln -s ../ant.properties ant.properties
